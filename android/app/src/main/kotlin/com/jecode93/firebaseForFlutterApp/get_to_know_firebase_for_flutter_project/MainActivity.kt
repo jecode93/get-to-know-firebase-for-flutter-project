f9e44a73738902e75348581859bb56fe0b2017e4
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-10-03 10:54:47 -0400

    First commit of the get to know firebase for flutter project pratice.

---
+++ android/app/src/main/kotlin/com/jecode93/firebaseForFlutterApp/get_to_know_firebase_for_flutter_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jecode93.firebaseForFlutterApp.get_to_know_firebase_for_flutter_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

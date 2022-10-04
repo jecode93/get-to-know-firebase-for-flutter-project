cd421c76776cb01c34e04cbaf7395961b2620965
jec-ode93 <jeanemmanuelcadet@gmail.com>
2022-10-04 02:00:53 -0400

    Clean the app base project and add a sample homeScreen

---
+++ lib/Widgets/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Text('Firebase Project'),
+      ),
+    );
+  }
+}
